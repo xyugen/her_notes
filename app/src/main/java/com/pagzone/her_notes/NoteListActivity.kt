3f491cf9f40a156bb294548f3929e7e03d97f198
xyugen <alex.arias.jorym@gmail.com>
2024-03-21 21:28:08 +0800

    Initial commit

---
+++ app/src/main/java/com/pagzone/her_notes/NoteListActivity.kt
@@ -0,0 +1,13 @@
+package com.pagzone.her_notes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class NoteListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_note_list)
+    }
+}
