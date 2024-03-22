2303204d515f7e1d2746b31671360a412c82b2a9
xyugen <alex.arias.jorym@gmail.com>
2024-03-22 21:08:34 +0800

    feat: add note-taking page

--- app/src/main/java/com/pagzone/her_notes/NoteListActivity.kt
+++ app/src/main/java/com/pagzone/her_notes/NoteListActivity.kt
@@ -1,5 +1,7 @@
 package com.pagzone.her_notes
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
@@ -7,6 +9,7 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,20 +18,28 @@ class NoteListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_note_list)
+
+        // Apply padding to handle system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.note_list)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnMenu: Button = findViewById(R.id.menu_button)
 
-        // Set click listener using lambda expression
-        btnMenu.setOnClickListener{
-            performAnimation(it)
-        }
+        // Initialize buttons
+        val btnMenu: Button = findViewById(R.id.menu_button)
+        val btnAdd: Button = findViewById(R.id.add_button)
+
+        // Set click listeners
+        btnAdd.setOnClickListener { onClick(it, this) }
     }
 
-    private fun performAnimation(view: View) {
-        println("Performing animation")
+    private fun onClick(view: View, context: Context) {
+        when (view.id) {
+            R.id.add_button -> {
+                val intent = Intent(context, AddNoteActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
