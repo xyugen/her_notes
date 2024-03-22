f9b329abd9a3295d5836919291ff4ecee0b1ceab
xyugen <alex.arias.jorym@gmail.com>
2024-03-22 22:16:11 +0800

    feat: implement function to back button

--- app/src/main/java/com/pagzone/her_notes/AddNoteActivity.kt
+++ app/src/main/java/com/pagzone/her_notes/AddNoteActivity.kt
@@ -1,6 +1,9 @@
 package com.pagzone.her_notes
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,20 @@ class AddNoteActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Initialize buttons
+        val btnBack: Button = findViewById(R.id.btn_back)
+
+        // Set click listeners
+        btnBack.setOnClickListener { onClick(it, this) }
+    }
+
+    private fun onClick(view: View, context: Context) {
+        when (view.id) {
+            R.id.btn_back -> {
+                finish()
+            }
         }
     }
 }
