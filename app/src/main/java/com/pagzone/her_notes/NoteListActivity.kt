69910ef79e4128b1b26e73e00320c630b63f2d18
xyugen <alex.arias.jorym@gmail.com>
2024-03-22 19:18:50 +0800

    feat: add ripple effect to squircle

--- app/src/main/java/com/pagzone/her_notes/NoteListActivity.kt
+++ app/src/main/java/com/pagzone/her_notes/NoteListActivity.kt
@@ -1,13 +1,29 @@
 package com.pagzone.her_notes
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_note_list)
+        val btnMenu: Button = findViewById(R.id.menu_button)
+
+        // Set click listener using lambda expression
+        btnMenu.setOnClickListener{
+            performAnimation(it)
+        }
+    }
+
+    private fun performAnimation(view: View) {
+        println("Performing animation")
     }
 }
